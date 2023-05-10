a0e9fba7abf94d3d37f01575295c358a03ad85fb
uzaysozen <uzaysozen@gmail.com>
2023-05-10 22:05:46 +0300

    Made converter code compatible with Linux

--- python/src/converter/convert/to_pdf.py
+++ python/src/converter/convert/to_pdf.py
@@ -1,59 +1,57 @@
 import pika, json, tempfile, os
 from bson.objectid import ObjectId
-from docx2pdf import convert
-import PyPDF2
-
+import subprocess
 
 def start(message, fs_word_files, fs_pdfs, channel):
     message = json.loads(message)
-    
-    # Create a temporary file for the PDF
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    # word contents
-    doc = fs_word_files.get(ObjectId(message["word_fid"]))
-    
-    # Use the docx2pdf module to convert the Word file to PDF
-    pdf_file = convert(doc)
 
-    # Open the PDF file and the temporary file
-    with open(pdf_file, 'rb') as pdf, open(temp_file.name, 'wb') as temp:
-        # Use PyPDF2 to read the PDF and write it to the temporary file
-        reader = PyPDF2.PdfFileReader(pdf)
-        writer = PyPDF2.PdfFileWriter()
-        writer.addPage(reader.getPage(0))
-        writer.write(temp)
+    # Create a temporary file for the Word document
+    with tempfile.NamedTemporaryFile(suffix=".docx", delete=False) as temp_word_file:
+        temp_word_file.write(fs_word_files.get(ObjectId(message["word_fid"])).read())
+        temp_word_file_path = temp_word_file.name
+        print(f"Temporary Word file created: {temp_word_file_path}")
 
-    # Close the files and delete the PDF file
-    pdf.close()
-    temp.close()
-    os.remove(pdf_file)
+    try:
+        # Create a temporary file for the PDF
+        with tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as temp_pdf_file:
+            temp_pdf_file_path = temp_pdf_file.name
+            print(f"Temporary PDF file created: {temp_pdf_file_path}")
 
-    # The PDF file is now saved to a temporary file, which can be accessed using temp_file.name
-    print('PDF file saved to', temp_file.name)
-    
-    with open(temp_file.name, 'rb') as file:
-        # Store the file in GridFS
-        pdf_id = fs_pdfs.put(file, filename=f"{message['word_fid']}.pdf")
-        os.remove(temp_file.name)
-        # Print the ID of the uploaded file
-        print("PDF file uploaded with ID:", pdf_id)
-        
-    message["pdf_fid"] = str(pdf_id)
-    
-    try: 
-        channel.basic_publish(
-            exchange="",
-            routing_key=os.environ.get("PDF_QUEUE"),
-            body=json.dumps(message),
-            properties=pika.BasicProperties(
-                pika.spec.PERSISTENT_DELIVERY_MODE
-                )
-        )
-    except Exception as err:
-        fs_pdfs.delete(pdf_id)
-        return "failed to publish message"
-        
-    
-    
-   
-    
+        # Use unoconv to convert the Word file to PDF
+        command = ['unoconv', '-f', 'pdf', '-o', temp_pdf_file_path, temp_word_file_path]
+        subprocess.run(command, check=True)
+        print("Word to PDF conversion completed")
+
+        # Read the PDF file
+        with open(temp_pdf_file_path, "rb") as pdf_file:
+            pdf_data = pdf_file.read()
+            print("PDF file read")
+
+        # Save the PDF file to MongoDB
+        pdf_id = fs_pdfs.put(pdf_data)
+        print(f"PDF file saved to MongoDB with ID: {pdf_id}")
+
+        # Delete the temporary files
+        os.remove(temp_word_file_path)
+        os.remove(temp_pdf_file_path)
+        print("Temporary files deleted")
+
+        message["pdf_fid"] = str(pdf_id)
+
+        try:
+            channel.basic_publish(
+                exchange="",
+                routing_key=os.environ.get("PDF_QUEUE"),
+                body=json.dumps(message),
+                properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE, content_type='application/json')
+            )
+            print("Message published successfully")
+        except Exception as err:
+            fs_pdfs.delete(pdf_id)
+            print("Failed to publish message", err)
+            return "Failed to publish message"
+    except subprocess.CalledProcessError as e:
+        print(f"Error converting Word to PDF: {e}")
+        return "Failed to convert Word to PDF"
+
+    print("Conversion completed successfully")
