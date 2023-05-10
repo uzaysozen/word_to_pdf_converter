37115d8f05726ab41f538402bc37d1e22db3665c
uzaysozen <uzaysozen@gmail.com>
2023-05-10 22:02:26 +0300

    Fixed type error in auth server.py

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -3,14 +3,14 @@ from flask import Flask, request
 from flask_mysqldb import MySQL
 
 server = Flask(__name__)
-mysql = MySQL(server)
-
 #config
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+server.config["MYSQL_PORT"] = int(str(os.environ.get("MYSQL_PORT")))
+
+mysql = MySQL(server)
 
 @server.route("/login", methods=["POST"])
 def login():
