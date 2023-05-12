276d491e5543fa8a07f00dfba3372d88ac2f5640
uzaysozen <uzaysozen@gmail.com>
2023-05-12 23:28:26 +0300

    Added download endpoint to gateway code

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -1,16 +1,25 @@
 import os, gridfs, pika, json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storage import util
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = "mongodb://host.docker.internal:27017/word_files"
 
-mongo = PyMongo(server)
+mongo_word = PyMongo(
+    server, 
+    uri= "mongodb://host.docker.internal:27017/word_files"
+)
 
-fs = gridfs.GridFS(mongo.db)
+mongo_pdf = PyMongo(
+    server, 
+    uri= "mongodb://host.docker.internal:27017/pdfs"
+)
+
+fs_word = gridfs.GridFS(mongo_word.db)
+fs_pdfs = gridfs.GridFS(mongo_pdf.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
@@ -30,12 +39,15 @@ def upload():
     
     access = json.loads(access)
     
+    if err:
+        return err
+    
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
         
         for _, f in request.files.items():
-            err = util.upload(f, fs, channel, access)
+            err = util.upload(f, fs_word, channel, access)
             
             if err:
                 return err
@@ -46,7 +58,28 @@ def upload():
     
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+    
+    if err:
+        return err
+    
+    access = json.loads(access)
+    
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+        
+        if not fid_string:
+            return "file id is required", 400
+        
+        try:
+            out = fs_pdfs.get(ObjectId(fid_string))
+            return send_file(out, download_name=f'{fid_string}.pdf')
+        
+        except Exception as err:
+            print(err)
+            return "internal server error", 500        
+        
+    return "not authorized", 401
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
